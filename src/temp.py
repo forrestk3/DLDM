c0b89a482443e3a6941112ecc1f87f30d5c0b041
hu-bing <forrest.k3@gmail.com>
2020-07-07 22:21:40 +0800

    get host ip from packet_in

--- src/temp.py
+++ src/temp.py
@@ -3,15 +3,14 @@ from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
-
 from ryu.ofproto import ofproto_v1_3
-
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
 from ryu.lib.packet import arp
 from ryu.lib.packet import ipv4
 from ryu.lib.packet import icmp
 
+#edited in sublime
 class IcmpResponder(app_manager.RyuApp):
     OFP_VERSIONS=[ofproto_v1_3.OFP_VERSION]
     def __init__(self,*args, **kwargs):
