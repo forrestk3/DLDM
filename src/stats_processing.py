cb0b6234cf8da17356151a05bf23a4f71669e062
hu-bing <forrest.k3@gmail.com>
2020-07-22 22:18:24 +0800

    使用自定义类存储流表信息,并对前一时刻内容进行存储

--- src/stats_processing.py
+++ src/stats_processing.py
@@ -1,10 +1,38 @@
 import queue
 import json
 
+# 类,函数使用驼峰命名；变量使用蛇形命名
+
 def hello():
     print('i am in stats_processing module,oh yeah!!!')
 
-stats = {}
+stats = {} #存储过滤后的流表消息
+set_pre_flow_entry = set() # 前一个时刻的流表
+set_flow_entry = set() # 当前流表集
+
+# 定义几个键常量
+key_OFPFlowStatsReply = 'OFPFlowStatsReply'
+key_body = 'body'
+key_OFPFlowStats = 'OFPFlowStats'
+key_match = 'match'
+key_OFPMatch = 'OFPMatch'
+key_oxm_fields = 'oxm_fields'
+key_OXMTlv = 'OXMTlv'
+key_priority = 'priority'
+
+# flow id key
+key_ipv4_src = 'ipv4_src'
+key_ipv4_dst = 'ipv4_dst'
+key_ip_proto = 'ip_proto'
+key_udp_src = 'udp_src'
+key_udp_dst = 'udp_dst'
+key_tcp_src = 'tcp_src'
+key_tcp_dst = 'tcp_dst'
+
+# flow feature key
+key_duration_sec = 'duration_sec'
+key_packet_count = 'packet_count'
+key_byte_count = 'byte_count'
 
 # stats_msg为一个列表，它的每个元素是一个字典
 # 该字典只有一个元素：键为dpid，值为msg.to_jsondict()
@@ -12,9 +40,17 @@ stats = {}
 def process(stats_msg):
     print(json.dumps(stats_msg))
     stats = filterIcmpAndArp(stats_msg)
-    print(json.dumps(stats))
-    
 
+    print('alen:',len(set_flow_entry))
+    print('blen:',len(set_pre_flow_entry))
+
+    print('current flow_entry')
+    for elements in set_flow_entry:
+        print(elements.__dict__)
+    print('previous flow_entry')
+    for elements in set_pre_flow_entry:
+        print(elements.__dict__)
+        
 def filterIcmpAndArp(stats_msg):
     # flow_stats为只有一个元素的字典，键为dpid，值为ev.msg.to_jsondict()
     stats_msg = stats_msg
@@ -22,20 +58,105 @@ def filterIcmpAndArp(stats_msg):
     bodys = []
     dpid = ''
     
+    # 保存前一个时期的流表,并将当前流表集清空
+    set_pre_flow_entry.clear()
+    for elements in set_flow_entry:
+        set_pre_flow_entry.add(elements)
+    set_flow_entry.clear()
+    
     for flow_stats in stats_msg:
         for dpid in flow_stats.keys():
-           # 这里不要丢键，否则会导致提取结果为空
-           OFPFlowStatsReply = flow_stats.get(dpid).get('OFPFlowStatsReply')
-           bodys = OFPFlowStatsReply.get('body')
-           if not bodys:
-               continue
-           udpAndTcpBody = []
-           for body in bodys:
-               # 这里又有一个键不要丢了
-               if body['OFPFlowStats']['priority'] == 5:
-                   udpAndTcpBody.append(body)
+            # 这里不要丢键，否则会导致提取结果为空
+            OFPFlowStatsReply = flow_stats.get(dpid).get(key_OFPFlowStatsReply)
+            bodys = OFPFlowStatsReply.get(key_body)
+            if not bodys:
+                continue
+            udpAndTcpBody = []
+            for body in bodys:
+                # 这里又有一个键不要丢了
+                if body[key_OFPFlowStats][key_priority] == 5:
+                    udpAndTcpBody.append(body)
+                    # 通过观察它的msg.to_json_dicts获取priority为5的所有匹配项
+                    # 并从匹配项中获取流id和统计值
+                    matches = body[key_OFPFlowStats][key_match][key_OFPMatch][key_oxm_fields]
 
-           OFPFlowStatsReply['body'] = udpAndTcpBody
-           flowStatsDict[dpid] = OFPFlowStatsReply
+                    sIP,dIP,sPort,dPort,pro = getFlowId(matches)
+
+                    tempFlow = FlowEntry(sIP,dIP,sPort,dPort,pro)
+                    tempFlow.duration_sec = body[key_OFPFlowStats][key_duration_sec]
+                    tempFlow.byte_count = body[key_OFPFlowStats][key_byte_count]
+                    tempFlow.packet_count = body[key_OFPFlowStats][key_packet_count]
+                    set_flow_entry.add(tempFlow)
+#                    set_pre_flow_entry.add(tempFlow)
+            OFPFlowStatsReply[key_body] = udpAndTcpBody
+            flowStatsDict[dpid] = OFPFlowStatsReply
+
     return flowStatsDict
+
+# 参数为oxm_fields列表，它的每一项都是一个OXMTlv格式的流表匹配项
+def getFlowId(oxm_fields):
+    sIP = None
+    dIP = None
+    sPort = None
+    dPort = None
+    proto = None
+
+    
+    for element_OXMTlv in oxm_fields:
+        # print('**********************')
+        # print(element_OXMTlv)
+        # print('**********************')
         
+        dict_OXMTlv = element_OXMTlv.get(key_OXMTlv)
+        
+        if dict_OXMTlv.get('field') == key_ipv4_src:
+            sIP = dict_OXMTlv.get('value')
+        elif dict_OXMTlv.get('field') == key_ipv4_dst:
+            dIP = dict_OXMTlv.get('value')
+        elif dict_OXMTlv.get('field') == key_udp_src or dict_OXMTlv.get('field') == key_tcp_src:
+            sPort = dict_OXMTlv.get('value')
+        elif dict_OXMTlv.get('field') == key_udp_dst or dict_OXMTlv.get('field') == key_tcp_dst:
+            dPort = dict_OXMTlv.get('value')
+        elif dict_OXMTlv.get('field') == key_ip_proto:
+            proto = dict_OXMTlv.get('value')
+        else:
+            continue
+    return sIP,dIP,sPort,dPort,proto
+
+
+class FlowEntry:
+    sIP = None
+    dIP = None
+    sPort = None
+    dPort = None
+    proto = None
+
+    duration_sec = 0
+    byte_count = 0
+    packet_count = 0
+
+    def __init__(self,sIP = None,dIP = None,sPort = None,dPort = None,proto = None):
+        self.sIP = sIP
+        self.dIP = dIP
+        self.sPort = sPort
+        self.dPort = dPort
+        self.proto = proto
+
+    def getSIP(self):
+        return self.sIP
+
+    def getDIP(self):
+        return self.dIP
+
+    def isSymFlow(self,other):
+        return self.sIP == other.dIP and self.dIP == other.sIP and self.sPort == other.dPort and self.dPort == other.sPort and self.proto == other.proto
+    
+    def __eq__(self,other):
+        if isinstance(other,self.__class__):
+            return self.sIP == other.getSIP() and self.dIP == other.getDIP() and self.sPort == other.sPort and self.dPort == other.dPort and self.proto == other.proto
+        else:
+            return False
+
+    def __hash__(self):
+        hash_str = self.sIP + self.dIP + str(self.sPort) + str(self.dPort) + str(self.proto)
+        return hash(hash_str)
--- src/temp.py
+++ src/temp.py
@@ -1,7 +1,45 @@
-import stats_processing
-import numpy as np
+class FlowEntry:
+    sIP = None
+    dIP = None
+    sPort = None
+    dPort = None
+    proto = None
 
-stats_processing.hello()
-i = 0
-for i in range(1,10):
-    print(np.random.poisson(lam=5,size=1))
+    def __init__(self,sIP = None,dIP = None,sPort = None,dPort = None,proto = None):
+        self.sIP = sIP
+        self.dIP = dIP
+        self.sPort = sPort
+        self.dPort = dPort
+        self.proto = proto
+
+    def isSymFlow(self,flow):
+        if flow.getSIP() == self.dIP and flow.getDIP() == self.sIP:
+            return True
+        return False
+
+    def getSIP(self):
+        return self.sIP
+
+    def getDIP(self):
+        return self.dIP
+    def __eq__(self,other):
+        if isinstance(other,self.__class__):
+            return self.sIP == other.getSIP() and self.dIP == other.getDIP() and self.sPort == other.sPort and self.dPort == other.dPort and self.proto == other.proto
+        else:
+            return False
+
+    def __hash__(self):
+        hash_str = self.sIP + self.dIP + str(self.sPort) + str(self.dPort) + str(self.proto)
+        return hash(hash_str)
+
+
+if __name__ == '__main__':
+    # print('oh yeah!!!')
+    flow = FlowEntry(sIP='1',dIP='2', sPort=3, dPort=4, proto=5001)
+    flow2 = FlowEntry(sIP='1',dIP='2', sPort=3, dPort=4, proto=5001)
+    # print(flow.getDIP())
+    print(flow.__eq__(flow2))
+    my_set = set()
+    my_set.add(flow2)
+    my_set.add(flow)
+    print(my_set,len(my_set))
